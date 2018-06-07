5cf18051e17cfba727869d0d50c5e1e8b1bf858c
pangdun <32231779+changdalao@users.noreply.github.com>
2018-06-07 23:07:37 +0800

    生产者-消费者模型（线程信号量）

--- 线程/pc.c
+++ 线程/pc.c
@@ -4,24 +4,26 @@
 #include<semaphore.h>
 #include<pthread.h>
 
-sem_t *blackSem;//生产者
-sem_t *dataSem;//消费者
+sem_t blackSem;//生产者
+sem_t dataSem;//消费者
 
 #define NUM 128
 
 int running[NUM];//环形队列
+
 void* consume()//消费者
 {
     srand((unsigned int)time);
     int data;
     int step=0;
     while(1){
-        sem_wait(blackSem);//p
+        sem_wait(&dataSem);//p
         data=running[step];
         printf("consume is done,data is %d\n",data);
         step++;
         step%=128;
-        sem_post(dataSem);
+        sleep(1);
+        sem_post(&blackSem);
     }
 }
 void* produce()//生产者
@@ -30,21 +32,21 @@ void* produce()//生产者
     int data;
     int step=0;
     while(1){
-        sem_wait(dataSem);
+        sem_wait(&blackSem);
         data=rand()%1024;
         running[step]=data;
         printf("produce is done,data is %d\n",data);
         step++;
         step%=128;
-        sem_post(blackSem);
+        sem_post(&dataSem);
         sleep(1);
     }    
 }
 int main()
 {
     //初始化信号量
-    sem_init(blackSem,0,NUM);
-    sem_init(dataSem,0,0);
+    sem_init(&blackSem,0,NUM);
+    sem_init(&dataSem,0,0);
     
     //创建线程
     pthread_t t1,t2;
@@ -56,7 +58,7 @@ int main()
     pthread_join(t2,NULL);
 
     //销毁信号量
-    sem_destroy(blackSem);
-    sem_destroy(dataSem);
+    sem_destroy(&blackSem);
+    sem_destroy(&dataSem);
     return 0;
 }
